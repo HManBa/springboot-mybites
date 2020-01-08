3e1a6f5c2a4980498519d536e194fbb0df079653
huhao <634482239@qq.com>
2020-01-08 14:11:38 +0800

    添加加密信息

--- spring-boot-mybatis-xml/src/main/java/com/neo/controller/UserTagController.java
+++ spring-boot-mybatis-xml/src/main/java/com/neo/controller/UserTagController.java
@@ -1,6 +1,9 @@
 package com.neo.controller;
 
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import lombok.extern.slf4j.Slf4j;
 
 /*************************
 * @ClassName: UserTagController
@@ -9,7 +12,17 @@ import org.springframework.web.bind.annotation.RestController;
 * @date 2019年12月24日 上午9:21:59
 *
 **************************/
+@Slf4j
 @RestController
 public class UserTagController {
 
+	public void GetUserInfo0(@RequestParam(value = " password") String value ) {
+		
+		
+		try {
+			
+		} catch (Exception e) {
+			log.error(e.getMessage());
+		}
+	}
 }
